77582536000c0178d83e73c7345fdf6f9d5415e9
shripad-kasar <shripadkasar@gmail.com>
2025-05-02 11:00:33 +0530

    By Shripad

---
+++ fdj188/src/fdj188/Test.java
@@ -0,0 +1,14 @@
+package fdj188;
+
+public class Test {
+
+	public void m1() {
+		System.out.println("M1-Method");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		t.m1();
+	}
+}

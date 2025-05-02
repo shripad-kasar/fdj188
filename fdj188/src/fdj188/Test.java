69800deb03177d401b84d57195d808e1bb38e160
shripad-kasar <shripadkasar@gmail.com>
2025-05-02 11:09:52 +0530

    Add m4 Method

--- fdj188/src/fdj188/Test.java
+++ fdj188/src/fdj188/Test.java
@@ -8,6 +8,9 @@ public class Test {
 	public void m2() {
 		System.out.println("M2-Method");
 	}
+	public void m4() {
+		System.out.println("M4-Method");
+	}
 	
 	public static void main(String[] args) {
 		

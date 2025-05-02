1d0a66ac0972202dad94c34b5d9e5bab801b86a2
shripad-kasar <shripadkasar@gmail.com>
2025-05-02 11:28:07 +0530

    addm3method

--- fdj188/src/fdj188/Test.java
+++ fdj188/src/fdj188/Test.java
@@ -8,6 +8,9 @@ public class Test {
 	public void m2() {
 		System.out.println("M2-Method");
 	}
+	public void m3() {
+		System.out.println("M3-Method");
+	}
 	
 	public static void main(String[] args) {
 		

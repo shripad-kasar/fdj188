c8a80c0f7396f809f35bbd2b2ec7b64f8ad60b3b
shripad-kasar <shripadkasar@gmail.com>
2025-05-02 11:02:14 +0530

    add m2 method

--- fdj188/src/fdj188/Test.java
+++ fdj188/src/fdj188/Test.java
@@ -5,10 +5,14 @@ public class Test {
 	public void m1() {
 		System.out.println("M1-Method");
 	}
+	public void m2() {
+		System.out.println("M2-Method");
+	}
 	
 	public static void main(String[] args) {
 		
 		Test t=new Test();
 		t.m1();
+		t.m2();
 	}
 }
